17a3c5f292dca6361e4986e3b22bc6d6570c6bf1
Lieven Doclo <lieven.doclo@aca-it.be>
2017-02-14 21:32:30 +0100

    code cleanup

--- app-impl/src/main/java/be/insaneprogramming/cleanarch/interactor/ListBuildingsImpl.java
+++ app-impl/src/main/java/be/insaneprogramming/cleanarch/interactor/ListBuildingsImpl.java
@@ -2,10 +2,12 @@ package be.insaneprogramming.cleanarch.interactor;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import be.insaneprogramming.cleanarch.boundary.ListBuildings;
 import be.insaneprogramming.cleanarch.entity.Building;
+import be.insaneprogramming.cleanarch.entity.Tenant;
 import be.insaneprogramming.cleanarch.entitygateway.BuildingEntityGateway;
 import be.insaneprogramming.cleanarch.requestmodel.ListBuildingsRequest;
 import be.insaneprogramming.cleanarch.responsemodel.BuildingResponseModel;
@@ -20,15 +22,28 @@ public class ListBuildingsImpl implements ListBuildings {
 
 	@Override
 	public void execute(ListBuildingsRequest request, Consumer<BuildingResponseModel> buildingResponseModelConsumer) {
-		final List<Building> buildings;
-		if(request.getNameStartsWith().isPresent()) {
-			buildings = buildingEntityGateway.findByNameStartingWith(request.getNameStartsWith().get());
-		} else {
-			buildings = buildingEntityGateway.findAll();
-		}
-		buildings.stream().map(b -> {
-						List<TenantResponseModel> tenantResponseModels = b.getTenants().stream().map(it -> new TenantResponseModel(it.getId(), it.getName())).collect(Collectors.toList());
-							return new BuildingResponseModel(b.getId(), b.getName(), tenantResponseModels);
-						}).forEach(buildingResponseModelConsumer);
+		final List<Building> buildings = findBuildings(request);
+		buildings.stream()
+				.map(getBuildingToBuildingResponseModelTransformer())
+				.forEach(buildingResponseModelConsumer);
+	}
+
+	private Function<Building, BuildingResponseModel> getBuildingToBuildingResponseModelTransformer() {
+		return b -> {
+				List<TenantResponseModel> tenantResponseModels = b.getTenants().stream()
+						.map(getTenantToTenantResponseModelTransformer())
+						.collect(Collectors.toList());
+				return new BuildingResponseModel(b.getId(), b.getName(), tenantResponseModels);
+			};
+	}
+
+	private Function<Tenant, TenantResponseModel> getTenantToTenantResponseModelTransformer() {
+		return it -> new TenantResponseModel(it.getId(), it.getName());
+	}
+
+	private List<Building> findBuildings(ListBuildingsRequest request) {
+		return request.getNameStartsWith()
+				.map(buildingEntityGateway::findByNameStartingWith)
+				.orElse(buildingEntityGateway.findAll());
 	}
 }
